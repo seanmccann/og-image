837cf56e07aadf1dfe98466a81db739d77d7a668
Sean McCann <sean89@gmail.com>
2021-02-02 15:03:13 -0800

    add abc logo

--- web/index.ts
+++ web/index.ts
@@ -91,7 +91,7 @@ interface FieldProps {
 const Field = ({ label, input }: FieldProps) => {
     return H('div',
         { className: 'field' },
-        H('label', 
+        H('label',
             H('div', {className: 'field-label'}, label),
             H('div', { className: 'field-value' }, input),
         ),
@@ -152,6 +152,7 @@ const imageDarkOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'ABCorps', value: 'http://seanmccann.s3.amazonaws.com/AlbertaCorporations.com.svg' },
 ];
 
 const widthOptions = [
