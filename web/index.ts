9c1c260323950a31d39dfd8fefeec62d915b42e6
Sean McCann <sean89@gmail.com>
2021-02-02 15:49:43 -0800

    new logo

--- web/index.ts
+++ web/index.ts
@@ -142,14 +142,14 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'ABCorps', value: 'https://seanmccann.s3.amazonaws.com/AlbertaCorporations.comBlack.svg' },
+    { text: 'ABCorps', value: 'https://seanmccann.s3.amazonaws.com/ablogo-black.svg' },
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
-    { text: 'ABCorps', value: 'https://seanmccann.s3.amazonaws.com/AlbertaCorporations.com.svg' },
+    { text: 'ABCorps', value: 'https://seanmccann.s3.amazonaws.com/ablogo.svg' },
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
